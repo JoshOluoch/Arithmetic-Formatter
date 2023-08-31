beb892a090297b6a9ab82ab1dc62ca9ad1c36a65
JoshOluoch <josholuoch101@gmail.com>
2023-08-31 18:09:53 +0300

    All error handling works. Input is validated. Returns appropriate errors.

--- arithmetic_arranger.py
+++ arithmetic_arranger.py
@@ -1,8 +1,29 @@
+import re
+
 def arithmetic_arranger(problems, work = False):
     
-    first_line = []
-    second_line = []
-    third_line = []
-    answers = []
+    first_line = ""
+    second_line = ""
+    dotted_line = ""
+    answers = ""
+
+    if len(problems) > 5:
+        return "Error: Too many problems."
+
+    for problem in problems:
+        firstNumber = problem.split(" ")[0]
+        operator = problem.split(" ")[1]
+        secondNumber = problem.split(" ")[2]
+
+        if len(firstNumber) > 4 or len(secondNumber) > 4 :
+            return "Error: Numbers cannot be more than four digits."
+
+        if(re.search("[*]",problem) or re.search("[/]",problem)):
+            return "Error: Operator must be '+' or '-'"
+            if(re.search("[^\s0-9.+-]",problem)):
+                return "Error: Numbers must only contain digits."
+        
+
+
     
-print(arithmetic_arranger(["32 + 8", "1 - 3801", "9999 + 9999", "523 - 49"]))
+print(arithmetic_arranger(["32 + 8", "1 - 3801", "999x + 9999", "523 - 49"]))
